07d39bbf3a6ec4ee9fe2e7061f1a90a965cb1f4e
Priya B <priya_saran@yahoo.com>
2022-11-30 18:41:29 -0800

    Added webpack, workbox plugins, css loader and babel to webpack.config.js, fixed index.js to pick up src-sw.js, commented out asset caching for now in service worker src-sw.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -17,13 +15,64 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+ 
+    // TODO: Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+
+      new WebpackPwaManifest({
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'A Progressive Text Editor Web App',
+        background_color: '#ffffff',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          }
+        ],
+        inject: true,
+        fingerprints: false
+      })  
     ],
 
     module: {
+      // TODO: Add CSS loaders and babel to webpack.
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              /* https://babeljs.io/docs/en/babel-preset-env */
+              /* allow newer JS to work on various target environments */
+              presets: ['@babel/preset-env'],
+              plugins: [
+                /* https://babeljs.io/docs/en/babel-plugin-proposal-object-rest-spread */
+                '@babel/plugin-proposal-object-rest-spread',
+                /* https://babeljs.io/docs/en/babel-plugin-transform-runtime */
+                /* allow babel to reuse helper code and reduce code size */
+                '@babel/transform-runtime'
+              ]
+            }
+          }
+        }
       ],
     },
   };
