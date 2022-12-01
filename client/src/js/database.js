b91b296e7fb7cd3ba9580f2569fd9965686672a1
Priya B <priya_saran@yahoo.com>
2022-11-30 16:21:53 -0800

    added putDB and getDB functions to update and retrieve the text from indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jateDb', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) =>  {
+  console.log('putDb');
+  const jateDb = await openDB('jateDb', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put();
+  const result = await request;
+  console.log("content saved to the database", result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  console.log('getDb');
+  const jateDb = await openDB('jateDb', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log("content returned from the database", result);
+  return result;
+}  
 
 initdb();
